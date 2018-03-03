4790b58fe2c915040818ee1cf6ec5dd55d289172
shavez1998 <36901922+shavez1998@users.noreply.github.com>
2018-03-03 16:31:59 +0100

    Build 1.2 -> Menu Layout

--- app/src/main/java/com/example/shavez/schooldiary/MainActivity.java
+++ app/src/main/java/com/example/shavez/schooldiary/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Hallo  SCHOOL PROJECT",Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_menu);
+        Toast.makeText(this,"HALLO SCHOOL PROJECT",Toast.LENGTH_LONG).show();
     }
 }
